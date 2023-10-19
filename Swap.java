1e9a281972ba357a8440ce9b274e0f3660070db4
Shubham5336 <148366238+Shubham5336@users.noreply.github.com>
2023-10-19 22:56:49 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Swap {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter number");
+
+        int a=sc.nextInt(),b=sc.nextInt();
+
+       a=a+b;
+
+       b=a-b;
+
+       a=a-b;
+
+       System.out.println("swap values of a&b are"+a+b);
+
+    }
+
+}
